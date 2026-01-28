39d16910dd861f7d8abb749319e0c124e282d7bb
nrjtvbd <147473582+nrjtvbd@users.noreply.github.com>
2026-01-28 23:41:07 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,28 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/vnd.apple.mpegurl");
-
-function get_data($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'AynaOTT/1.2.1');
-    curl_setopt($ch, CURLOPT_REFERER, 'https://aynaott.com/');
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
-
-// ১. সোর্স থেকে লিঙ্ক সংগ্রহ
-$source = get_data("http://mrgifytv.pages.dev/bpl/tsports/index.m3u8");
+$source = file_get_contents("http://mrgifytv.pages.dev/bpl/tsports/index.m3u8");
 preg_match('/https?:\/\/[^\s]+/', $source, $matches);
-
 if (isset($matches[0])) {
-    // ২. সরাসরি লিঙ্কটি রিডাইরেক্ট করে দেখা (Vercel-এ এটিই সবচেয়ে বেশি কাজ করে)
     header("Location: " . trim($matches[0]));
     exit;
-} else {
-    echo "Stream Not Found";
 }
 ?>
